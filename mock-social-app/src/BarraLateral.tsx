2dbd44a16a580660a5750e61f18357e547449bc8
1w3111923GualMarcos <111923@tecnicatura.frc.utn.edu.ar>
2022-03-12 23:40:20 -0300

    barraLateral
    Estilos2

--- mock-social-app/src/BarraLateral.tsx
+++ mock-social-app/src/BarraLateral.tsx
@@ -3,29 +3,43 @@ import React from 'react'
 import './BarraLateral.css'
 
 export default function BarraLateral() {
-    return(
-    <div className='barralateral'>
-        <div className='barralateral__arriba'>
-            <img src='https://photy.org/photos/blue-slide-background-wide.jpg' alt='' />
-            <Avatar className='barralateral_avatar' />
-            <h2>Marcos Gual</h2>
-            <h4>marcos.gual@outlook.com</h4>
-        </div>
 
-        <div className="barralateral__stats">
-            <div className="barralateral__stat">
-                <p>Quién te vio</p>
-                <p className="barralateral__numero">156</p>
-            </div>
-            <div className="barralateral__stat">
-                <p>Vistas del post</p>
-                <p className="barralateral__numero">93</p>
-            </div>
+    const itemReciente = (topic) => ( //no siempre hace falta tener un componente, podemos tener una función que haga el renderizado
+        <div className="barralateral">
+            <span className="barralateral__hash">#</span>
+            <p>{topic}</p>
         </div>
+    )
 
-        <div className="barralateral__abajo">
-            <p>Reciente</p>
+    return (
+        <div className='barralateral'>
+            <div className='barralateral__arriba'>
+                <img src='https://photy.org/photos/blue-slide-background-wide.jpg' alt='' />
+                <Avatar className='barralateral_avatar' />
+                <h2>Marcos Gual</h2>
+                <h4>marcos.gual@outlook.com</h4>
+            </div>
+
+            <div className="barralateral__stats">
+                <div className="barralateral__stat">
+                    <p>Quién te vio</p>
+                    <p className="barralateral__numero">156</p>
+                </div>
+                <div className="barralateral__stat">
+                    <p>Vistas del post</p>
+                    <p className="barralateral__numero">93</p>
+                </div>
+            </div>
+
+            <div className="barralateral__abajo">
+                <p>Reciente</p>
+                {itemReciente('react')}
+                {itemReciente('react')}
+                {itemReciente('react')}
+                {itemReciente('react')}
+                {itemReciente('react')}
+                {itemReciente('react')}
+            </div>
         </div>
-    </div>
     );
 }
